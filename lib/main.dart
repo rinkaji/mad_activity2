a25e908bb8356c087b962b3ce527e43d648e84a0
unknown <rinkajhin@gmail.com>
2025-03-03 08:24:44 +0800

    assignment

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:act2/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainScreen());
+  }
+}
